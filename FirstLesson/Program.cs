1213b3f56d1e8fc2be2f7dc3d9792376c1977fbf
Sergey <suspect555list.ru>
2023-03-28 13:29:10 +0300

    Second_Task Solved

--- FirstLesson/Program.cs
+++ FirstLesson/Program.cs
@@ -16,3 +16,39 @@ else
 }
 */
 
+/*Задача No3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+3 -> Среда
+5 -> Пятница*/
+
+Console.WriteLine("Enter number of day");
+
+int a = int.Parse(Console.ReadLine());
+
+if (a == 1)
+{
+    Console.WriteLine("Monday");
+}
+if (a == 2)
+{
+    Console.WriteLine("Tuesday");
+}
+if (a == 3)
+{
+    Console.WriteLine("Wednesday");
+}
+if (a == 4)
+{
+    Console.WriteLine("Thursday");
+}
+if (a == 5)
+{
+    Console.WriteLine("Friday");
+}
+if (a == 6)
+{
+    Console.WriteLine("Saturday");
+}
+if (a == 7)
+{
+    Console.WriteLine("Sunday");
+}
