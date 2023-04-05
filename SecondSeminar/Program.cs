3c90cf1fba5a3aa615d86b590c8345811bb07f17
Sergey <suspect555list.ru>
2023-04-05 21:21:36 +0300

    10th task solwed

--- SecondSeminar/Program.cs
+++ SecondSeminar/Program.cs
@@ -1,17 +1,61 @@
-﻿/*Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-a = 5; b = 7 -> max = 7
-a = 2 b = 10 -> max = 10
-a = -9 b = -3 -> max = -3
+﻿/*
+int a = 5;
+
+int b = 7;
+
+int c = 9; 
+
+int g = 8;
+
+int result1 = Calculate(a,b);
+Show(a,b);
+
+int result2 = Calculate(c,g);
+Show(c,g);
+
+Console.WriteLine(result1);
+Console.WriteLine(result2);
+
+int Calculate(int a, int b)
+{
+    int result = a * (a * b) -5;
+
+    return result;
+}
+
+void Show(int a, int b)
+{
+    Console.WriteLine($"{a} , {b}");
+}
 */
 
-int a = int.Parse(Console.ReadLine());
-int b = int.Parse(Console.ReadLine());
+/*Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+● 456 -> 46
+● 782 -> 72
+● 918 -> 98*/
 
-if(a>=b)
+Random random = new Random();
+
+int randomValue = random.Next(100, 1000);
+
+int lastDigit = randomValue % 10;
+int firstDigit = randomValue / 100;
+
+Console.WriteLine($"{randomValue}");
+Console.WriteLine($"{firstDigit}{lastDigit}");
+
+/*Console.WriteLine($"{randomValue}");
+Console.WriteLine($"{firstDigit}");
+Console.WriteLine($"{lastDigit}");
+
+
+if (lastDigit > firstDigit)
 {
-    Console.WriteLine($"a = {a}; b = {b} -> max = {a}");
+    Console.WriteLine(lastDigit);
 }
+
 else
-{
-    Console.WriteLine($"a = {a}; b = {b} -> max = {b}");
+{ 
+    Console.WriteLine(firstDigit);
 }
+*/
