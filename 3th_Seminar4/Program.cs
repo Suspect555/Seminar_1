39c835281d8fd095a2deab12f44210fb08cb6596
Sergey <suspect555list.ru>
2023-04-06 21:39:28 +0300

    task 22 solved

--- 3th_Seminar4/Program.cs
+++ 3th_Seminar4/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿// Задача No22. Работа в группах
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел
+// от 1 до N.
+// ● 5 -> 1, 4, 9, 16, 25.
+// ● 2 -> 1,4
+
+
+Console.WriteLine("enter number");
+int N = int.Parse(Console.ReadLine());
+
+for (int i = 1; i < N; i++)   
+{
+    Console.WriteLine (i*i);
+}
