3522e7bcfc768cd6e929b4eb7763588dc62e020e
Sergey <suspect555list.ru>
2023-04-06 20:03:12 +0300

    task 13 solved

--- 3th_Seminar1/Program.cs
+++ 3th_Seminar1/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("enter x");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("enter y");
+int y = int.Parse(Console.ReadLine());
+
+int result = GetNumberOfQuarters(x,y);
+
+if (result == 0)
+{
+    Console.WriteLine("x or y equal zero. data is wrong");
+    return;
+}
+
+Console.WriteLine(result);
+
+int GetNumberOfQuarters(int x, int y)
+{
+    if(x>0 && y>0)
+    {
+        return 1;
+
+    } 
+    if (y>0 && x<0)
+    {
+
+    return 2;
+    }
+    if (x<0 && y<0)
+    {
+
+    return 3;
+    }
+
+    if (x>0 && y <0)
+    {
+
+    return 4;
+    }
+    return 0;
+}
+
