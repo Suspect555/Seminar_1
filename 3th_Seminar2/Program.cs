633452383a95abb913add9d6d8b051a262fb531e
Sergey <suspect555list.ru>
2023-04-06 20:19:49 +0300

    task 18 solved

--- 3th_Seminar2/Program.cs
+++ 3th_Seminar2/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("enter NumberOfQuarter");
+int num = int.Parse(Console.ReadLine());
+
+string result = GetCorArr(num);
+
+// if (result == 0)
+// {
+//     Console.WriteLine("x or y equal zero. data is wrong");
+//     return;
+// }
+
+Console.WriteLine(result);
+
+string GetCorArr(int num)
+{
+    if(num == 1)
+    {
+        return "x>0 && y>0";
+
+    } 
+    if (num == 2)
+    {
+
+    return "y>0 && x<0";
+    }
+    if (num == 3)
+    {
+
+    return "x<0 && y<0";
+    }
+
+    if (num == 4)
+    {
+
+    return "x>0 && y <0";
+    }
+    return "invalid Quarter";
+}
