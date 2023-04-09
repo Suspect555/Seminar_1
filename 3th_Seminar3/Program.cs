b0cdfe1f6fc58300801af6f522159371bce2f4e9
Sergey <suspect555list.ru>
2023-04-09 15:12:17 +0300

    3th seminar edited

--- 3th_Seminar3/Program.cs
+++ 3th_Seminar3/Program.cs
@@ -1,4 +1,8 @@
 ﻿
+// Задача No21. Работа в группах
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+// ● A (3,6); B (2,1) -> 5,09
+// ● A (7,-5); B (1,-1) -> 7,21
 
 Console.WriteLine("enter x_A");
 int x_A = int.Parse(Console.ReadLine());
@@ -21,7 +25,7 @@ Console.WriteLine($"A ({x_A}, {y_A}); B ({x_B}, {y_B}) -> {result}");
 
 double GetRange(int x_A, int y_A, int x_B, int y_B)
 {
-    result = (x_B - x_A) * (x_B - x_A) + (y_B - y_B) * (y_B - y_B); // ab = √(xb - xa)2 + (yb - ya)2
+    result = Math. Sqrt((x_B - x_A) * (x_B - x_A) + (y_B - y_A) * (y_B - y_A)); // ab = √(xb - xa)2 + (yb - ya)2
     
     return result;
 }
