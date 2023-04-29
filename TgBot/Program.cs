e6bb50387ef185591887b664e631b91956e812a3
Sergey <suspect555list.ru>
2023-04-29 12:26:40 +0300

    7th_Seminar created

--- TgBot/Program.cs
+++ TgBot/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.InputFiles;
+
+TelegramBotClient client = new TelegramBotClient("5982116056:AAGkxgv_l_vEoDxhb5urQSRZkDFqF5MECl8");
+
+User user = await client.GetMeAsync();
+
+Console.WriteLine(user.Username);
+
+while (true)
+{
+    Update[] updates = await client.GetUpdatesAsync();
+
+    for (int i = 0; i < updates.Length; i++)
+    {
+        Console.WriteLine(updates[i].Message.Text);
+        Console.WriteLine(updates[i].Message.From.FirstName);
+
+        await client.SendTextMessageAsync(updates[i].Message.From.Id, "test text");
+    }
+
+    if (updates.Length != 0)
+    {
+        updates = await client.GetUpdatesAsync(updates[updates.Length - 1].Id + 1);
+    }
+}
